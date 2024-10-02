1c8f4719a40995cd4f4a6d2acec1c5d487f9e4c1
kenantonico <antonicokenquitayen@gmail.com>
2024-10-02 23:40:38 +0800

    Added: a controller and a view "Index"
    Modified the layout
    
    added two H2 tags "Hi"

---
+++ CarRentalWebsite/Controllers/CustomersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalWebsite.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
