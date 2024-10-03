a9bc92bcd2119087905fa4fe712c0f167b528f2e
kenantonico <antonicokenquitayen@gmail.com>
2024-10-03 16:21:24 +0800

    Add: Shared Folder and ViewImports and ViewStart in Customers Area
    Setup the navigation bar for Customer side
    Setup the Navigation Bar and Routing

---
+++ CarRentalWebsite/Areas/Customers/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalWebsite.Areas.Customers.Controllers
+{
+    [Area("Customers")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
