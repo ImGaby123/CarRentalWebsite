6eecbe9198f223a42ebf406724b04991a1e00568
kenantonico <antonicokenquitayen@gmail.com>
2024-10-03 17:25:33 +0800

    Add: HomeController
    Added in AreaCustomer Controllers

---
+++ CarRentalWebsite/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalWebsite.Areas.Customer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
