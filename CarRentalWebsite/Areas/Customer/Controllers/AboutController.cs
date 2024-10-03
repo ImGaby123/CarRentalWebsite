ccbe1dffd41bee4caa537492bfa1d24f5dbb8761
kenantonico <antonicokenquitayen@gmail.com>
2024-10-03 19:54:25 +0800

    A: About Conttoller and View
    Created Initial File for both

---
+++ CarRentalWebsite/Areas/Customer/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalWebsite.Areas.Customer.Controllers
+{
+    [Area("About")]
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
