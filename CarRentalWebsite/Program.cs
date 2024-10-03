30322034ee45aef04c7f3ebde76fff83ccf55990
ImGaby123 <gabyplaza123@gmail.com>
2024-10-03 11:48:31 +0800

    - Added a Controller and a View  in the Admin Area - Modified Index view, Layout, Program.cs
    I have setup the link for the Admin area so that you could access it in the Default Index View.

--- CarRentalWebsite/Program.cs
+++ CarRentalWebsite/Program.cs
@@ -35,6 +35,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapAreaControllerRoute(
+    name: "MyAreaAdmin",
+    areaName: "Admin",
+    pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
