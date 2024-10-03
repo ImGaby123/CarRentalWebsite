8e9d8041d9ed84146792f7eb8a7eb7a85995f642
kenantonico <antonicokenquitayen@gmail.com>
2024-10-03 19:49:29 +0800

    A: Services Controller and View A: Services CSS
    Finished initial design for services

---
+++ CarRentalWebsite/Areas/Customer/Controllers/ServicesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalWebsite.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ServicesController : Controller
+    {
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
+}
