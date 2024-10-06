bf0d6a733745a42b45260d5ff31489bda5ffc3a5
ImGaby123 <gabyplaza123@gmail.com>
2024-10-06 21:29:37 +0800

    Added OwnerController: Created a new controller for owner page

---
+++ CarRentalWebsite/Areas/Admin/Controllers/OwnerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalWebsite.Areas.Admin.Controllers
+{
+    public class OwnerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
