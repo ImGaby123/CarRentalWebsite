0825eb72fa4dd408e6074c8ee348cfcd9436ac97
kenantonico <antonicokenquitayen@gmail.com>
2024-10-17 00:27:38 +0800

    M: Index Files
    Satisfied by design

---
+++ CarRentalWebsite/Areas/Customer/Models/Index.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalWebsite.Areas.Customer.Models
+{
+    public class Index
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string PickUpLocation { get; set; }
+
+        public string ReturnLocation { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FromDate { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime FromTime { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime UntilDate { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime UntilTime { get; set; }
+    }
+}
