606567ab362c7ac73952b440ebbc1431183e304e
kenantonico <antonicokenquitayen@gmail.com>
2024-10-16 21:27:35 +0800

    A: Contact Model M: Contact view and css
    Finished design and models for contact

---
+++ CarRentalWebsite/Areas/Customer/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRentalWebsite.Areas.Customer.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Questions { get; set; }
+    }
+}
