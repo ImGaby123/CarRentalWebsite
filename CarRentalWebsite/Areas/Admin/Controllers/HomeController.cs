b0c0609afbb0785f866f6233a0d2794db15b5db3
ImGaby123 <gabyplaza123@gmail.com>
2024-10-17 08:50:27 +0800

    Added a Dashboard class in Admin/Models. Created a model class in preparation for the dashboard page datas.
    Modified Home Controller.
    Setup an initial method for computing the value for "Totals" Data in the dashboard.
    
    Modified Index View of Admin/Controller/Home.
    placed a code that calls Model Dashboard in Line 1

--- CarRentalWebsite/Areas/Admin/Controllers/HomeController.cs
+++ CarRentalWebsite/Areas/Admin/Controllers/HomeController.cs
@@ -9,5 +9,22 @@ namespace CarRentalWebsite.Areas.Admin.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+
+        public IActionResult Index(Models.Dashboard d) {
+
+            double totalRevenue = 500000.00;
+            double totalACustomer = 10500.00;
+            double totalRentedCars = 5600.00;
+
+
+            // Model Var assignment Here
+            d.Total_Revenue = totalRevenue;
+            d.Total_ACustomer = totalACustomer;
+            d.total_RentedCars = totalRentedCars;
+
+            return View(d);
+        }
     }
 }
---
+++ CarRentalWebsite/Areas/Admin/Models/Dashboard.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRentalWebsite.Areas.Admin.Models
+{
+    public class Dashboard
+    {
+
+        // Dashboard Totals Data Summary 
+        public double Total_Revenue { get; set; }
+        public double Total_ACustomer {  get; set; }
+        public double total_RentedCars {  get; set; }
+
+
+    }
+}
