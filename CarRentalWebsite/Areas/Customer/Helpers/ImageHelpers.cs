abc3b3cba0866da7716e631a7cc4049117d40912
kenantonico <antonicokenquitayen@gmail.com>
2024-10-17 23:43:28 +0800

    A: Helpers folder in Customer Area A: ImageHelpers.cs -- it sets default consistent sizing for images in img folder
    M: Reservation view
    --with images that are consistent sizing

---
+++ CarRentalWebsite/Areas/Customer/Helpers/ImageHelpers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Encodings.Web;
+
+namespace CarRentalWebsite.Areas.Customer.Helpers
+{
+    public static class ImageHelpers
+    {
+        public static IHtmlContent ImageWithSize(this IHtmlHelper htmlHelper, string imagePath, int width = 398, int height = 246, string alt = "Image")
+        {
+            var imgTag = new TagBuilder("img");
+            imgTag.Attributes["src"] = imagePath;
+            imgTag.Attributes["alt"] = alt;
+            imgTag.Attributes["width"] = width.ToString();
+            imgTag.Attributes["height"] = height.ToString();
+
+            using (var writer = new StringWriter())
+            {
+                imgTag.WriteTo(writer, HtmlEncoder.Default);
+                return new HtmlString(writer.ToString());
+            }
+        }
+    }
+}
