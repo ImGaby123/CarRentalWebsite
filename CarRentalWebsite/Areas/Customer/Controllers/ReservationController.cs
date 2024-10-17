8958cfd9a03ff26175d3c40bd8cd78d355907bd4
kenantonico <antonicokenquitayen@gmail.com>
2024-10-17 21:21:51 +0800

    M: All Reservation MVC files -- Finished initial form for reservation page

--- CarRentalWebsite/Areas/Customer/Controllers/ReservationController.cs
+++ CarRentalWebsite/Areas/Customer/Controllers/ReservationController.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CarRentalWebsite.Areas.Customer.Models;
 
 namespace CarRentalWebsite.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class ReservationController : Controller
     {
+        [HttpGet]
         public IActionResult Reservation()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SubmitReservation(Reservation model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToAction("Success");
+            }
+
+       
+            return View("Reservation", model);
+        }
+
+        public IActionResult Success()
         {
             return View();
         }
--- CarRentalWebsite/Areas/Customer/Models/Reservation.cs
+++ CarRentalWebsite/Areas/Customer/Models/Reservation.cs
@@ -1,28 +1,40 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarRentalWebsite.Areas.Customer.Models
 {
     public class Reservation
     {
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
         public string PhoneNumber { get; set; }
 
-        public string PickUpLocation { get; set; }
+        [Required(ErrorMessage = "Pick-up location is required")]
+        public string PickupLocation { get; set; }
 
+        [Required(ErrorMessage = "Return location is required")]
         public string ReturnLocation { get; set; }
 
+        [Required(ErrorMessage = "From date is required")]
         [DataType(DataType.Date)]
-        public DateTime FromDate { get; set; }
+        public DateTime FromDate { get; set; } = DateTime.Today;
 
+        [Required(ErrorMessage = "From time is required")]
         [DataType(DataType.Time)]
         public DateTime FromTime { get; set; }
 
+        [Required(ErrorMessage = "Until date is required")]
         [DataType(DataType.Date)]
-        public DateTime UntilDate { get; set; }
+        public DateTime UntilDate { get; set; } = DateTime.Today;
 
+        [Required(ErrorMessage = "Until time is required")]
         [DataType(DataType.Time)]
         public DateTime UntilTime { get; set; }
     }
