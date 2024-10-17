0245f1bba2f252b438c54642f49257f972de5403
ImGaby123 <gabyplaza123@gmail.com>
2024-10-17 09:22:03 +0800

    Modified Transactions Model. Setup variables for Total Earnings summary

--- CarRentalWebsite/Areas/Admin/Models/Transactions.cs
+++ CarRentalWebsite/Areas/Admin/Models/Transactions.cs
@@ -2,5 +2,17 @@
 {
     public class Transactions
     {
+
+        // Total Earnings Data Summary 
+        public double total_Earnings { get; set; }
+        public double total_AmountPaid { get; set; }
+        public double total_Refunds { get; set; }
+
+        public Transactions()
+        {
+            total_Earnings = 1000000;
+            total_AmountPaid = 50000;
+            total_Refunds = 30000;
+        }
     }
 }
