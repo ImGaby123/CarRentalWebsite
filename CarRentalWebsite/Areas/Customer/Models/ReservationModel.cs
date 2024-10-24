bc5b3ae34fd0efa11ac6a4c25764a3f12bbaf2b3
kenantonico <antonicokenquitayen@gmail.com>
2024-10-24 09:06:08 +0800

    M: Reserve CSS --finished designing reservation form

--- CarRentalWebsite/Areas/Customer/Models/ReservationModel.cs
+++ CarRentalWebsite/Areas/Customer/Models/ReservationModel.cs
@@ -5,26 +5,35 @@ namespace CarRentalWebsite.Areas.Customer.Models
 {
     public class ReservationModel
     {
+       [Required]
         public int ReservationId { get; set; }
+        [Required]
         public required string Name { get; set; }
+        [Required]
 
         public required string Email { get; set; }
+        [Required]
 
         public required string PhoneNumber { get; set; }
-
+        [Required]
         public required string PickupLocation { get; set; }
+        [Required]
 
         public required string ReturnLocation { get; set; }
+        [Required]
 
         [DataType(DataType.Date)]
         public required DateTime FromDate { get; set; } = DateTime.Today;
-      
+
+        [Required]
         [DataType(DataType.Time)]
         public required DateTime FromTime { get; set; }
 
+        [Required]
         [DataType(DataType.Date)]
         public required DateTime UntilDate { get; set; } = DateTime.Today;
 
+        [Required]
         [DataType(DataType.Time)]
         public required DateTime UntilTime { get; set; }
     }
